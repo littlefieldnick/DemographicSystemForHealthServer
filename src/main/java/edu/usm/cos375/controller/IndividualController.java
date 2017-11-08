50b8d1fc3f32a493f7e971ef805ff914a1f4b1a6
littlefieldnick <nickolas.littlefield@maine.edu>
2017-11-07 21:13:40 -0500

    Moved RestController using ResponseEntity to a separate class.
    Separated out the RestController using the ResponseEntity out from the
    Controller that doesn't use ResponseEntity.

--- src/main/java/edu/usm/cos375/controller/IndividualController.java
+++ src/main/java/edu/usm/cos375/controller/IndividualController.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -30,31 +26,13 @@ public class IndividualController {
 	public List<Individual> getAll(){
 		return individualService.getAllIndividuals();
 	}
-	
-//	public ResponseEntity<List<Individual>> getAll(){
-//		List<Individual> individuals = individualService.getAllIndividuals();
-//		if(individuals == null || individuals.isEmpty()) {
-//			return new ResponseEntity<List<Individual>>(HttpStatus.NO_CONTENT);
-//		}
-//
-//		return new ResponseEntity<List<Individual>>(individuals, HttpStatus.OK);
-//	}
 
 	//Find an individual by their id
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	public Individual get(@PathVariable("id") long id) {
 		return individualService.getIndividual(id);
 	}
-	
-//    public ResponseEntity<Individual> get(@PathVariable("id") long id){
-//        Individual individual = individualService.getIndividual(id);
-//
-//        if (individual == null){
-//            return new ResponseEntity<Individual>(HttpStatus.NOT_FOUND);
-//        }
-//
-//        return new ResponseEntity<Individual>(individual, HttpStatus.OK);
-//    }
+
 
 	//Add an individual 
 	@RequestMapping(method = RequestMethod.POST)
@@ -65,17 +43,7 @@ public class IndividualController {
 			individualService.add(individual);
 		}
 	}
-//	public ResponseEntity<Void> create(@RequestBody Individual individual, UriComponentsBuilder ucBuilder){
-//		if(individualService.getIndividual(individual.getId()) != null) {
-//			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-//		}
-//
-//		individualService.add(individual);
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setLocation(ucBuilder.path("/individuals/{id}").buildAndExpand(individual.getId()).toUri());
-//
-//		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-//	}
+
 
 	//Update an existing user
 	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
@@ -90,36 +58,9 @@ public class IndividualController {
 		
 		return individualService.getIndividual(id);
 	}
-//	public ResponseEntity<Individual> update(@PathVariable int id, @RequestBody Individual individual){
-//		Individual updateIndividual = individualService.getIndividual(id);
-//
-//		if(updateIndividual == null) {
-//			return new ResponseEntity<Individual>(HttpStatus.NOT_FOUND);
-//		}
-//
-//		
-//		individualService.update(individual);
-//		updateIndividual = individualService.getIndividual((long) id);
-//
-//		return new ResponseEntity<Individual>(updateIndividual, HttpStatus.OK);
-//	}
-
 	
 	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable("id") long id) {
 		individualService.remove(id);
 	}
-	
-//	public ResponseEntity<Void> delete(@PathVariable("id") long id){
-//		Individual individual = individualService.getIndividual(id);
-//		if (individual == null){
-//			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-//		}
-//
-//		individualService.remove(id);
-//		return new ResponseEntity<Void>(HttpStatus.OK);
-//	}
-
-
-
 }
