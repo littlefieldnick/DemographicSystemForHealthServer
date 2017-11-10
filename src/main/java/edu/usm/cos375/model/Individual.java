8270d972d404fee2efedc21175c43600a0c4b4fa
littlefieldnick <nickolas.littlefield@maine.edu>
2017-11-10 18:30:56 -0500

    Updates to the individual class.
    Added relationship, membership and residency sets. Some basic validation
    added.

--- src/main/java/edu/usm/cos375/model/Individual.java
+++ src/main/java/edu/usm/cos375/model/Individual.java
@@ -1,32 +1,54 @@
 package edu.usm.cos375.model;
 
 import java.util.Date;
+import java.util.Set;
+import java.util.HashSet;
+
+import javax.validation.constraints.*;
+import javax.persistence.*;
+
 /*
  * Individual class represents a single individual in the initial census. 
  * 
  * TODO: Membership, residency, pregnancy, relationship, and membership episodes, will need to be added once implemented. 
+ * TODO: Implement additional validation.
  */
 
 public class Individual {
 	private long id;		//For purposes of temporary in memory storage
+	
+	@Past
 	private Date dob;
 	private String dobAspect;
+	
+	@NotNull
 	private String firstName;
+	
+	@NotNull
 	private String middleName;
+	
+	@NotNull
 	private String lastName;
+	
+	@NotNull
 	private String gender;
+	private Individual mother;
+	private Individual father;
 	private int age;
+	private String ageUnits;
 	private String phoneNumber;
 	private String otherPhoneNumber;
 	private String languagePreference;
 	private String pointOfContactName;
 	private String pointOfContactPhoneNumber;
+	private String dip;
+	private String membershipStatus;
 	private String nationality;
-
-	public Individual() {
-		// TODO Auto-generated constructor stub
-	}
-
+	private Set <Residency> residencies = new HashSet<>();
+	private Set<Relationship> relationshipA = new HashSet<>();
+	private Set<Relationship> relationshipB = new HashSet<>();
+	private Set<Membership> memberships = new HashSet<>();
+	
 	public long getId() {
 		return id;
 	}
@@ -81,6 +103,22 @@ public class Individual {
 
 	public void setGender(String gender) {
 		this.gender = gender;
+	}
+	
+	public Individual getMother() {
+		return mother;
+	}
+
+	public void setMother(Individual mother) {
+		this.mother = mother;
+	}
+
+	public Individual getFather() {
+		return father;
+	}
+
+	public void setFather(Individual father) {
+		this.father = father;
 	}
 
 	public int getAge() {
@@ -138,5 +176,60 @@ public class Individual {
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
 	}
+	
+	public Set<Residency> getResidencies() {
+		return residencies;
+	}
 
+	public void setResidencies(Set<Residency> residencies) {
+		this.residencies = residencies;
+	}
+
+	public Set<Relationship> getRelationshipA() {
+		return relationshipA;
+	}
+
+	public void setRelationshipA(Set<Relationship> relationshipA) {
+		this.relationshipA = relationshipA;
+	}
+
+	public Set<Relationship> getRelationshipB() {
+		return relationshipB;
+	}
+
+	public void setRelationshipB(Set<Relationship> relationshipB) {
+		this.relationshipB = relationshipB;
+	}
+
+	public Set<Membership> getMemberships() {
+		return memberships;
+	}
+
+	public void setMemberships(Set<Membership> membership) {
+		this.memberships = membership;
+	}
+
+	public String getAgeUnits() {
+		return ageUnits;
+	}
+
+	public void setAgeUnits(String ageUnits) {
+		this.ageUnits = ageUnits;
+	}
+
+	public String getDip() {
+		return dip;
+	}
+
+	public void setDip(String dip) {
+		this.dip = dip;
+	}
+
+	public String getMembershipStatus() {
+		return membershipStatus;
+	}
+
+	public void setMembershipStatus(String membershipStatus) {
+		this.membershipStatus = membershipStatus;
+	}	
 }
